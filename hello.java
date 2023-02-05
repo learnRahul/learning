28a8f6dd6309d0840c9ab6f8090ca6f475a972d3
learnRahul <94137401+learnRahul@users.noreply.github.com>
2023-02-05 09:37:50 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is Hello World program test!"); 
+        System.out.println("This is Hello program test!"); 
     }
 }
